20bc3855e2be1a0be03481812482ec77c2d88c81
allen-lhj <liuhuanjun767@gmial.com>
2022-12-19 17:20:02 +0800

    assect management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
-
+import './style.css'
+import Icon from './icon.jpeg'
 function component () {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['hello', 'webpack'], '');
+  element.classList.add('hello');
 
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,26 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // loading css
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // loading image
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // loading font
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
